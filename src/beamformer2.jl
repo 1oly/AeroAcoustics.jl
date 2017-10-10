212d24cee31b5c35a9a80b6c4dfd2114ec5f12f1
ollyl <ollyl@dtu.dk>
2017-10-10 14:15:58 +0200

    Improvements to level correction in beamformer2

--- src/beamformer2.jl
+++ src/beamformer2.jl
@@ -1,26 +1,38 @@
-@traceable function beamformer2(E::Environment,C::Constants,V::SteeringMatrix;psf::Bool=false)
+function beamformer2(E::Environment,C::Constants,V::SteeringMatrix;psf::Bool=false)
     b = Array{Float64}(E.N,E.Nf)
 
     # Compute beamforming
     for j in 1:length(E.f)
         for i in eachindex(E.D0)
-            b[i,j] = abs(V.v[:,i,j]'*E.CSM[j,:,:]*V.v[:,i,j])
+            b[i,j] = real(V.v[:,i,j]'*E.CSM[j,:,:]*V.v[:,i,j])
+        end
+    end
+    return reshape(b,E.Nx,E.Ny,E.Nf)
+end
+
+function beamformer2(E::Environment,C::Constants,V::SteeringMatrix;psf::Bool=true)
+    b = Array{Float64}(E.N,E.Nf)
+    PSF = similar(b)
+    gm = Array{Complex{Float64}}(E.M)
+
+    cent = round(Int64,E.N/2)+1
+
+    # Compute beamforming
+    for j in 1:length(E.f)
+        for i in eachindex(E.D0)
+            b[i,j] = real(V.v[:,i,j]'*E.CSM[j,:,:]*V.v[:,i,j])
         end
     end
 
-    if psf
-        PSF = similar(b)
-        for j in 1:length(E.f)
-            gm = vec(V.v[:,round(Int64,E.N/2)+1,j])
-            for i in eachindex(E.D0)
-                for m in 1:E.M
-                    PSF[i,j] = (E.M*E.D0[i]/E.D[i,m])^2*abs(V.v[:,i,j]'*gm)^2
-                end
-            end
+    # Compute PSF
+    for j in 1:length(E.f)
+        gm = E.M*V.v[:,cent,j].*(E.D0[cent]./E.D[cent,:]) # Compensate to get correct level.
+        for i in eachindex(E.D0)
+            PSF[i,j] = abs(V.v[:,i,j]'*gm)^2
+            #for m in 1:E.M
+                #PSF[i,j] = (E.M*E.D0[i]/E.D[i,m])^2*abs(V.v[:,i,j]'*gm)^2
+            #end
         end
-
-        return reshape(b,E.Nx,E.Ny,E.Nf),reshape(PSF,E.Nx,E.Ny,E.Nf)
-    else
-        return reshape(b,E.Nx,E.Ny,E.Nf)
     end
+    return reshape(b,E.Nx,E.Ny,E.Nf),reshape(PSF,E.Nx,E.Ny,E.Nf)
 end
