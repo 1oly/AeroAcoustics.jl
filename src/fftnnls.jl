b288846184054e961f37f6522566204c04a12b12
ollyl <ollyl@dtu.dk>
2017-10-10 14:02:29 +0200

    Added fftnnls method

---
+++ src/fftnnls.jl
@@ -0,0 +1,38 @@
+function fftnnls{T}(psf::Array{T,2},b::Array{T,2},X0::Array{T,2},maxit::Int64)
+    X = copy(X0)
+    Nx,Ny = size(X)
+    obj = zeros(maxit)
+    center = round(Int64,Nx/2)+1,round(Int64,Ny/2)+1
+    Fps = fft(circshift(psf,center))
+    FpsT = fft(circshift(psf',center))
+	alpha = 0.0
+    k = 0
+    while k < maxit
+        k += 1
+		r = real(ifft(Fps.*fft(X)))-b
+		obj[k] = 0.5*vecnorm(r)^2
+	    g = real(ifft(FpsT.*fft(r)))
+		w = g
+		w[(x .== 0.0) .& (w .> 0.0)] = 0.0
+		g = real(ifft(FpsT.*fft(w)))
+		alpha = dot(vec(g),vec(r))/dot(vec(g),vec(g))
+		X = max.(0.0,real(X-alpha*w))
+    end
+    return X,obj
+end
+
+function fftnnls{T}(psf::Array{T,3},b::Array{T,3},X0::Array{T,2},maxit::Int64)
+    X = similar(b)
+    Nx,Ny,Nf = size(b)
+    obj = zeros(maxit,Nf)
+    for i in 1:Nf
+        X[:,:,i],obj[:,i] = fista(psf[:,:,i],b[:,:,i],X0,maxit)
+    end
+
+    # Warm-start:
+    #Threads.@threads for i in Nf:-1:1
+    #    Y[:,:,i] = fista(psf[:,:,i],b[:,:,i],Xprev,maxit)
+    #    Xprev = reshape(Y[:,:,i],Nx,Ny)
+    #end
+    return X,obj
+end
