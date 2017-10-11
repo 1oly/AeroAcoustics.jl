f6d932071eddddc1d5998a4d03bc99a78dda5c64
ollyl <ollyl@dtu.dk>
2017-10-11 14:47:26 +0200

    Added PSF function

--- src/AeroAcoustics.jl
+++ src/AeroAcoustics.jl
@@ -1,4 +1,4 @@
-#__precompile__()
+__precompile__()
 module AeroAcoustics
 using NLsolve, Distances
 
@@ -18,6 +18,7 @@ export Constants,
        beamformer2,
        beamformersetup,
        steeringvectors,
+       pointspreadfunction,
        fista,
        fftnnls,
        shear,
