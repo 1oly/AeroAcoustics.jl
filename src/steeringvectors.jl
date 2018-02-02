18e7af81fd3b9a384c410c41d28c865627d64262
ollyl <ollyl@dtu.dk>
2018-02-02 12:18:28 +0100

    WIP Uniform flow

--- src/steeringvectors.jl
+++ src/steeringvectors.jl
@@ -31,9 +31,12 @@ function steeringvectors(E::Environment{T},C::Constants{T},kind::Shear) where T 
     t = Array{T}(E.M,E.N)
     xm = Array{T}(3)
     xm[3] = 0.0
+    vi = Array{Complex{T}}(E.M,E.N,length(E.f))
     for i in eachindex(E.D0)
         for m in 1:E.M
             xm[1:2] .= E.micgeom[1:2,m]-E.Rxy[1:2,i]
+            #f_c(x,fvec) = shear!(x,fvec,xm,C) # TODO: Maybe slightly faster
+            #res = nlsolve(f_c,[1.;1.;1.])
             res = nlsolve((x,fvec)->shear!(x,fvec,xm,C), [ 1.; 1.; 1.])
             xi = res.zero
             r1 = sqrt(xi[1]^2+xi[2]^2+xi[3]^2)  # From 0 to shear interface
@@ -63,6 +66,26 @@ function steeringvectors(E::Environment{T},C::Constants{T},kind) where T <: Abst
     return nothing
 end
 
+# Still WIP:
+function steeringvectors(E::Environment{T},C::Constants{T},kind::Uniform) where T <: AbstractFloat
+    kw = 2pi*E.f/((1-C.Ma^2)*C.c)
+    vi = Array{Complex{T}}(E.M,E.N,length(E.f))
+    # Rescale distances to take doppler shift into account
+    Rxy = E.Rxy./sqrt.(1-C.Ma.^2)
+    micgeom = E.micgeom./sqrt.(1-C.Ma.^2)
+    D = pairwise(Euclidean(), Rxy, micgeom)
+    D0 = colwise(Euclidean(), Rxy, [0,0,0])
+    for j in 1:length(E.f)
+        for i in eachindex(E.D0)
+            for m in 1:E.M
+                vi[m,i,j] = (1/E.M)*(D[i,m]/D0[i])*exp(-im*kw[j]*((1-C.Ma)*D[i,m]-D0[i]))
+            end
+        end
+    end
+    return SteeringMatrix(vi,kind)
+end
+
+# If dispatch of typeinstace
 #function steeringvectors(E::Environment{T},C::Constants{T},kind::AbstractString) where T <: AbstractFloat
 #    steeringvectors(E,C,typeinstance(kind))
 #end
