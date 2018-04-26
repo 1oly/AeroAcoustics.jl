d754c543e49bb9cc80f9f16e44d6597451980a22
ollyl <ollyl@dtu.dk>
2018-04-26 14:09:11 +0200

    Update build script to get pipeline running

--- build_script.jl
+++ build_script.jl
@@ -1,4 +1,4 @@
 Pkg.update()
-Pkg.clone(pwd(), \"AeroAcoustics\")
+Pkg.clone(pwd(), "AeroAcoustics")
 #Pkg.clone("https://gitlab.windenergy.dtu.dk/ollyl/AeroAcoustics.jl","AeroAcoustics")
 Pkg.build("AeroAcoustics")
