ae960bbffedb9fcce964c55760f6970fcfae672e
ollyl <ollyl@dtu.dk>
2019-03-25 11:06:53 +0100

    Amiet

--- src/AeroAcoustics.jl
+++ src/AeroAcoustics.jl
@@ -2,6 +2,7 @@ module AeroAcoustics
 using Distances
 using LinearAlgebra # LinAlg in julia 1.0
 using Statistics
+using NLsolve
 using Parameters
 import DSP
 
@@ -21,7 +22,6 @@ export Environment,
        damas!
 
 
-
 include("types.jl")
 include("steeringvectors.jl")
 include("csm.jl")
@@ -36,5 +36,32 @@ function SPL(p::Array{T}) where T <: Real
     return s
 end
 SPL(p::Number) = p > 0.0 ? 10*log10(p/4e-10) : -350.0
+
+function shear!(fvec,xi,xm,Ma,h)
+    a = sqrt(xi[1]^2+(1-Ma^2)*(xi[2]^2+xi[3]^2))
+    b = norm(xm-xi)
+    fvec[1] = (1/a)*xi[1]-(1/b)*(1-Ma^2)*(xm[1]-xi[1]) - Ma
+    fvec[2] = (1/a)*xi[2]-(1/b)*(xm[2]-xi[2])
+    fvec[3] = xi[3] - h
+end
+
+function propagation_time(E::Environment)
+    @unpack N,M,micgeom,Rxy,Ma,h,c = E
+    ta = Array{Float64,2}(undef,N,M)
+    xm = zeros(3,M)
+    for n = 1:N
+        xm[1:2,:] .= micgeom[1:2,:] .- Rxy[1:2,n]
+        for m = 1:M
+            res = nlsolve((fvec,x)->shear!(fvec,x,xm[:,m],Ma,h),[.1,.1,h])
+            xi = res.zero
+            r1 = norm(xi)
+            d = xi[1]*Ma*c/r1
+            c1 = d + sqrt(d^2+c^2-(Ma*c)^2)
+            r2 = norm(xm[:,m]-xi)
+            ta[n,m] = r1/c1 + r2/c
+        end
+    end
+    return ta
+end
 
 end
--- src/types.jl
+++ src/types.jl
@@ -11,23 +11,48 @@ Base.axes(A::FreqArray) = axes(A.arr)
 Base.getindex(A::FreqArray, i::Int) = A.arr[i]
 Base.getindex(A::FreqArray{T, N}, I::Vararg{Int, N}) where {T, N} = A.arr[I...]
 
+"""
+    Environment
+
+The Environment struct is required for most methods and defines the geometrical
+setup, constants, and stores the relevant data together. The fields are
+
+# Arguments
+
+*Required:*
+
+- `micgeom::Array{T,2}`: (x,y,z) coordinates for microhone array.
+- `z0::Real`: distance to source plane from microphone array.
+- `CSM::FreqArray`: Cross-spectral matrix, size `M x M x Nf`, where `M` is the number of microphones and `Nf` is the number of frequency bins.
+
+*Optional:*
+
+- `flim::Tuple=extrema(CSM.fc)`: Frequency limits (fmin,fmax).
+- `Nx::Integer=21`: Number of computational gridpoint in x direction.
+- `Ny::Integer=21`: Number of computational gridpoint in y direction.
+- `xlim::Tuple=(-1.,1.)`: Cartesian x-coordinate limits.
+- `ylim::Tuple=(-1.,1.)`: Cartesian y-coordinate limits.
+- `c::Real=343.`: Speed of sound [m/s].
+- `Ma::Real=0.0`: Mach number.
+- `h::Real=0.0`: Distance to shear layer (Amiet correction) should be supplied when `Ma != 0`.
+"""
 @with_kw mutable struct Environment <: AeroAcousticType
     micgeom::Matrix{<:AbstractFloat}
-    z0::Float64
+    z0::Real
     CSM::FreqArray
     flim::NTuple{2,Real} = extrema(CSM.fc)
-    Cinds = (CSM.fc.>=flim[1]) .& (CSM.fc.<=flim[2])
-    fn = CSM.fc[Cinds]
-    Nf::Int = length(fn)
     Nx::Int = 21
     Ny::Int = 21
-    @assert Nx>0 && Ny>0 && z0>0 "Nx, Ny, z0 must be positive"
     xlim::NTuple{2,Real} = (-1.,1.)
     ylim::NTuple{2,Real} = (-1.,1.)
     c::Real = 343.0 # Speed of sound
-    Ma::Real = 0.0 # Flow Mach speed
+    Ma::Real = 0.0 # Flow Mach speed TODO: Generalize to NTuple{3,Real}
+    h::Real = 0.0 # Distance to shear layer (used for Shear layer correction)
     ### Compute extra parameters
+    Cinds = (CSM.fc.>=flim[1]) .& (CSM.fc.<=flim[2])
+    fn = CSM.fc[Cinds]
     M::Int = size(micgeom,2)
+    Nf::Int = length(fn)
     N = Nx*Ny
     rx = range(xlim[1],stop = xlim[2], length = Nx)
     ry = range(ylim[1],stop = ylim[2], length = Ny)
