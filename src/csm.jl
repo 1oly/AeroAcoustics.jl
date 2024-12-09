9a1f6d22ad79ecf2449b7ddc800803f5fbead0c8
Oliver Lylloff <ollyl@dtu.dk>
2024-12-09 11:26:06 +0100

    bugfix csm with threads

--- src/csm.jl
+++ src/csm.jl
@@ -37,8 +37,8 @@ end
 Calculate cross-spectral matrix from time series `t` which is `S x M` dimensional,
 where `S` is the number of samples and `M`the number of microphones.
 """
-function csm(t::AbstractArray{T};n=1024,noverlap=div(n,2),fs=1,win=DSP.hanning(n),scaling="spectrum") where T <: AbstractFloat
-    csm(flat_t(t);n=n,noverlap=noverlap,fs=fs,win=win,scaling=scaling)
+function csm(t::AbstractArray{T};n=1024,noverlap=div(n,2),fs=1,win=DSP.hanning(n),scaling="spectrum",multi_thread=true) where T <: AbstractFloat
+    csm(flat_t(t);n=n,noverlap=noverlap,fs=fs,win=win,scaling=scaling,multi_thread=true)
 end
 
 function csm(t::Vector{<:AbstractVector{T}};n=1024,noverlap=div(n,2),fs=1,win=DSP.hanning(n),scaling="spectrum",multi_thread=true) where T <: AbstractFloat
